3c20fd313bf7576dc57617e72fa8c12f09a28a40
Chris Delnooz <cdelnooz@micro-x.com>
2018-01-25 23:05:02 +1030

    Added preliminary support for references into various .net framework versions

--- make/lang/cs.mk
+++ make/lang/cs.mk
@@ -58,8 +58,27 @@ endif
 CSC ?= $(CS_BINDIR)csc.exe
 
 ALL_CS_FLAGS = $(VARIANT.CS_FLAGS) $(OS.CS_FLAGS) $(ARCH.CS_FLAGS) $(LOCAL.CS_FLAGS) \
-    $(TARGET.CS_FLAGS) $(CS_FLAGS)
+    $(TARGET.CS_FLAGS) $(PROJECT.CS_FLAGS) $(CS_FLAGS)
 
+# All assemblies that are references need to be passed to the compiler; for the moment
+# these rules assume references are given by full path! This because of the variety
+# of .Net Framework versions that exist and can be used in mixed fashion.
+# the directories can be overridden by the project specific file
+# the local makefiles should define framework_<version>_lib with all the dll files 
+# the assembly needs from the specific framework version. Note: if all assemblies require the
+# same framework, the project mk-file could globally set this.
+# In addition, local files can set LOCAL.CS_REFS for non-system references
+framework_root ?= '/c/Program Files (x86)/Reference Assemblies/Microsoft/Framework/.NETFramework/'
+framework_3.5 ?= $(framework_root)v3.5/
+framework_4.5 ?= $(framework_root)v4.5/
+framework_4.5.1 ?=$(framework_root)v4.5.1/
+
+TARGET.CS_REFS = $(framework_3.5_lib:%=-reference:$(framework_3.5)%.$(LIB_SUFFIX)) \
+  $(framework_4.5_lib:%=-reference:$(framework_4.5)%.$(LIB_SUFFIX)) \
+  $(framework_4.5.1_lib:%=-reference:$(framework_4.5.1)%.$(LIB_SUFFIX))
+
+ALL_CS_REFS = $(VARIANT.CS_REFS) $(OS.CS_REFS) $(ARCH.CS_REFS) $(LOCAL.CS_REFS) \
+    $(TARGET.CS_REFS) $(PROJECT.CS_REFS) $(CS_REFS)
 
 #
 # build: --Build all the cs sources that have changed.
@@ -69,7 +88,7 @@ build-cs: $(archdir)/$(TARGET)
 
 $(archdir)/$(TARGET): $(CS_SRC) | $(archdir)
 	$(ECHO_TARGET)
-	$(CSC) $(ALL_CS_FLAGS) $^ "-out:$@"
+	$(CSC) $(ALL_CS_FLAGS) $(ALL_CS_REFS) $^ "-out:$@"
 
 #
 # TODO: install: --install cs binaries and libraries.
@@ -99,7 +118,7 @@ src:	src-cs
 src-cs:
 	$(ECHO_TARGET)
 	@mk-filelist -f $(MAKEFILE) -qn CS_SRC $$(find . -path ./obj -prune -o -type f -name '*.$(CS_SUFFIX)' -print)
-	@mk-filelist -f $(MAKEFILE) -qn CS_MAIN_SRC $$(grep -l $(CS_MAIN_RGX) ''*.$(CS_SUFFIX)'')
+	@mk-filelist -f $(MAKEFILE) -qn CS_MAIN_SRC $$(grep -l $(CS_MAIN_RGX) ''*.$(CS_SUFFIX)'' 2>/dev/null)
 
 #
 # todo: --Report "unfinished work" comments in Java files.
