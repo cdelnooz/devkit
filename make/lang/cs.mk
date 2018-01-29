8c1e8424529cff99aa12c632e6f6a9dc780019d9
Chris Delnooz <cdelnooz@micro-x.com>
2018-01-29 22:25:54 +1030

    implemented building of .resources files with resgen.exe

--- make/lang/cs.mk
+++ make/lang/cs.mk
@@ -19,6 +19,8 @@ CS_SUFFIX ?= cs
 #       one of the os/*.mk
 LIB_SUFFIX ?= dll
 EXE_SUFFIX ?= exe
+# TODO: is this also supported by mono?
+RSX_SUFFIX ?= resx
 
 # this regex searches for the main-function in C#, which normally is:
 # static void Main
@@ -27,8 +29,10 @@ CS_MAIN_RGX ?= '^[ \t]*static[ \t]*void[ \t]Main'
 ifdef autosrc
     LOCAL_CS_SRC := $(shell find . -path ./obj -prune -o -type f -name '*.$(CS_SUFFIX)' -print)
     LOCAL_CS_MAIN := $(shell grep -l $(CS_MAIN_RGX) '*.$(CS_SUFFIX)')
+    LOCAL_RSX_SRC := $(shell find . -path ./obj -prune -o -type f -name '*.$(RSX_SUFFIX)' -print)
     CS_SRC ?= $(LOCAL_CS_SRC)
     CS_MAIN_SRC ?= $(LOCAL_CS_MAIN)
+		RSX_SRC ?= $(LOCAL_RSX_SRC)
 endif
 
 # these rules assume that the C# code is organised as follows:
@@ -61,13 +65,16 @@ ifdef APP_CONFIG
 endif
 
 CSC ?= $(CS_BINDIR)csc.exe
+RESGEN ?= $(RESGEN_BINDIR)resgen.exe
 
 ALL_CS_FLAGS = $(VARIANT.CS_FLAGS) $(OS.CS_FLAGS) $(ARCH.CS_FLAGS) $(LOCAL.CS_FLAGS) \
     $(TARGET.CS_FLAGS) $(PROJECT.CS_FLAGS) $(CS_FLAGS)
 
-# All assemblies that are references need to be passed to the compiler; for the moment
-# these rules assume references are given by full path! This because of the variety
-# of .Net Framework versions that exist and can be used in mixed fashion.
+ALL_RSX_FLAGS = $(VARIANT.RSX_FLAGS) $(OS.RSX_FLAGS) $(ARCH.RSX_FLAGS) $(LOCAL.RSX_FLAGS) \
+    $(TARGET.RSX_FLAGS) $(PROJECT.RSX_FLAGS) $(RSX_FLAGS)
+# All assemblies that are references need to be passed to the compiler; Because of the variety
+# of .Net Framework versions that exist and can be used in mixed fashion, the references
+# are passed to the compiler with full-path, thus enabling cherry picking across versions
 # How to use:
 # similar to languages, specify dotnet_frameworks. The names for each framework can be
 # freely chosen. For example:
@@ -82,13 +89,14 @@ ALL_CS_FLAGS = $(VARIANT.CS_FLAGS) $(OS.CS_FLAGS) $(ARCH.CS_FLAGS) $(LOCAL.CS_FL
 # then lastly, at the module level, one would define a variable <framework_name>.ref with
 # all references to use from that framework (without the extension). E.g.
 # v2_0.ref = mscorlib System System.Data
-TARGET.CS_REFS = $(foreach f,$(dotnet_frameworks),$($(f).ref:%=-reference:$($(f).dir)%.$(LIB_SUFFIX)))
+TARGET.CS_REFS = $(foreach f,$(dotnet_frameworks),$($(f).ref:%=-r:$($(f).dir)%.$(LIB_SUFFIX)))
 
 # besides the the system libs, which are just referenced, the local makefile can specify local.ref
 # which has librarires that will be copied to the local archdir (mimic the Visual Studio "copy local")
-# lastly, for local refs that do not need copying, a -reference can be added to LOCAL.CS_FLAGS directly
+# lastly, for local refs that do not need copying, a -r can be added to LOCAL.CS_FLAGS directly
 # TODO: filenames with spaces don't work with below expansion; work around: use the DOS name
-LOCAL.CS_REFS += $(local.ref:%=-reference:%.$(LIB_SUFFIX))
+# NOTE: when adding references, use -r rather than -reference so RESGEN won't fail
+LOCAL.CS_REFS += $(local.ref:%=-r:%.$(LIB_SUFFIX))
 
 # create build rules for local references
 define copy_local
@@ -105,25 +113,39 @@ $(patsubst %, $(archdir)/%.$(LIB_SUFFIX), $(notdir $(1))): $(1).$(LIB_SUFFIX)
 			$$(INSTALL_DATA) $$(?:%.$(LIB_SUFFIX)=%.xml) $$(@:%.$(LIB_SUFFIX)=%.xml))
 endef
 
+# dependency generation for resources files
+define make-deps-resx
+$(gendir)/$(notdir $(1:%.$(RSX_SUFFIX)=%.resources)): $1 | $(gendir)
+endef
+
 ALL_CS_REFS = $(VARIANT.CS_REFS) $(OS.CS_REFS) $(ARCH.CS_REFS) $(LOCAL.CS_REFS) \
     $(TARGET.CS_REFS) $(PROJECT.CS_REFS) $(CS_REFS)
-
+RESOURCES = $(addprefix $(gendir)/,$(notdir $(RSX_SRC:%.$(RSX_SUFFIX)=%.resources)))
 #
 # build: --Build all the cs sources that have changed.
 #
 build:		build-cs $(TARGET.CONFIG) $(foreach ref,$(local.ref),$(archdir)/$(notdir $(ref).$(LIB_SUFFIX)))
 build-cs: $(archdir)/$(TARGET)
 
-
+# for each of the local references, generate the build rules for copy-local behaviour
 $(foreach ref,$(local.ref),$(eval $(call copy_local,$(ref))))
 
-$(archdir)/$(TARGET): $(CS_SRC) | $(archdir)
+$(archdir)/$(TARGET): $(CS_SRC) $(RESOURCES)| $(archdir)
 	$(ECHO_TARGET)
-	$(CSC) $(ALL_CS_FLAGS) $(ALL_CS_REFS) $^ "-out:$@"
+	$(CSC) $(ALL_CS_FLAGS) $(ALL_CS_REFS) $(RESOURCES:%=-res:%) $(CS_SRC) "-out:$@"
 
 $(TARGET.CONFIG): $(APP_CONFIG)
 	$(INSTALL_DATA) $? $@
 
+# build the resources files
+$(foreach d, $(RSX_SRC), $(eval $(call make-deps-resx,$d)))
+# note: resgen.exe doesn't accept -compile (yay, consistency Microsoft!) so
+# we have to pass /compile. Msys by default will convert that to c:\msys64\compile.
+# to avoid that, we  need to set the MSYS_ARG_CONV_EXCL environment variable to include
+# /compile. This can be done in the project mk-file.
+%.resources :
+	$(ECHO_TARGET)
+	$(RESGEN) $(ALL_RSX_FLAGS) $(ALL_CS_REFS) /compile $<,$@
 #
 # TODO: install: --install cs binaries and libraries.
 #
@@ -136,11 +158,15 @@ uninstall-cs:
 #
 # clean: --Remove build outputs
 #
-distclean:	clean-cs
+distclean:	distclean-cs
 clean:	clean-cs
 clean-cs:
 	$(ECHO_TARGET)
 	$(RM) $(archdir)/*
+	$(RM) $(gendir)/*
+distclean-cs: clean-cs
+	$(ECHO_TARGET)
+	$(RM) -r bin obj
 
 #
 # src: --Update the CS_SRC macro.
@@ -153,6 +179,7 @@ src-cs:
 	$(ECHO_TARGET)
 	@mk-filelist -f $(MAKEFILE) -qn CS_SRC $$(find . -path ./obj -prune -o -type f -name '*.$(CS_SUFFIX)' -print)
 	@mk-filelist -f $(MAKEFILE) -qn CS_MAIN_SRC $$(grep -l $(CS_MAIN_RGX) ''*.$(CS_SUFFIX)'' 2>/dev/null)
+	@mk-filelist -f $(MAKEFILE) -qn RSX_SRC $$(find . -path ./obj -prune -o -type f -name '*.$(RSX_SUFFIX)' -print)
 
 #
 # todo: --Report "unfinished work" comments in Java files.
