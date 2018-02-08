b6404a68fd9fd8edf2af72a4e1e0b804d011562a
Chris Delnooz <cdelnooz@micro-x.com>
2018-02-08 11:49:56 +1030

    added nunit test runner

--- make/lang/vsproj.mk
+++ make/lang/vsproj.mk
@@ -1,15 +1,16 @@
 # build a Visual Studio Project with msbuild.
 # Seems a bit superfluous, as msbuild is a make alternative, but
 # this is useful when managing a larger project with make, whilst some subcomponents are done with Visual Studio & MSBuild.
-
-# MSBUILD_DIR can be set in project specific make include
 .PHONY: $(recursive-targets:%=%-vsproj)
 
+# MSBUILD_DIR can be set in project specific make include
 MSBUILD ?= $(MSBUILD_BINDIR)msbuild.exe
+LOG_FILE ?= $(ARCH)_$(VARIANT)_build.log
+
+TARGET.BUILD_FLAGS = -fl -flp:logfile=$(archdir)$(LOG_FILE)
 
 ALL_BUILD_FLAGS = $(VARIANT.BUILD_FLAGS) $(OS.BUILD_FLAGS) $(ARCH.BUILD_FLAGS) \
 	$(LOCAL.BUILD_FLAGS) $(TARGET.BUILD_FLAGS) $(PROJECT.BUILD_FLAGS)
-
 
 # protobuf generator
 PGEN ?= $(PGEN_BINDIR)protogen.exe
@@ -25,11 +26,11 @@ clean: clean-vsproj
 clean-vsproj:
 	$(MSBUILD) $< $(ALL_BUILD_FLAGS) -t:Clean
 	$(RM) $(PROTO_SRC:%=%.cs)
+	$(RM) $(archdir)$(LOG_FILE)
 
 distclean: distclean-vs
 distclean-vs:
 	$(RM) -r bin obj
-
 
 src: src-vsproj
 src-vsproj:
